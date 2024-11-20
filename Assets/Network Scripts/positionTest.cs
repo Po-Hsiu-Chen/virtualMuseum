59539b0443b2891fd7528354b4eb0b9e773975cd
pollychen5911 <pollychen3911@gmail.com>
2024-11-20 22:07:16 +0800

    加入各館資料, 離開按鈕

--- Assets/Network Scripts/positionTest.cs
+++ Assets/Network Scripts/positionTest.cs
@@ -3,119 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
-public class positionTest : MonoBehaviour
+public class PositionTest : MonoBehaviour
 {
-    // Start is called before the first frame update
     string projectName = "";
-    void Start()
-    {
-        
-    }
+    Vector3 targetPosition; // 目標位置
+    bool isChecking = false; // 防止多次檢查執行
 
-    // Update is called once per frame
     void Update()
     {
-        if(projectTP.flag1){
-            changePosition("project1");
+        if (projectTP.flag1)
+        {
+            ChangePosition("project1", new Vector3(-93.3f, 1.1f, 39));
         }
-        if(projectTP.flag2){
-            changePosition("project2");
+        if (projectTP.flag2)
+        {
+            ChangePosition("project2", new Vector3(-87.3f, 1.1f, 39));
         }
-        if(projectTP.flag3){
-            changePosition("project3");
+        if (projectTP.flag4)
+        {
+            ChangePosition("project4", new Vector3(-106.5f, 1.1f, 39));
         }
-        if(projectTP.flag4){
-            changePosition("project4");
-        }
-        if(projectTP.flag5){
-            changePosition("project5");
-        }
-    }
-
-    void changePosition(string project){
-        if(GetComponent<NetworkObject>().HasStateAuthority){
-            if(project == "project1"){
-                projectName = "project1";
-                transform.position = new Vector3(-74, 1.1f, 39);
-                Invoke("check", 0.2f);
-                projectTP.flag1 = false;
-            }
-            else if(project == "project2"){
-                Debug.Log("展場2");
-                projectName = "project2";
-                transform.position = new Vector3(-88, 1.1f, 39);
-                Invoke("check", 0.2f);
-                projectTP.flag2 = false;
-            }
-            else  if(project == "project3"){
-                projectName = "project3";
-                transform.position = new Vector3(-95, 1.1f, 39);
-                Invoke("check", 0.2f);
-                projectTP.flag3 = false;
-            }
-            else if(project == "project4"){
-                projectName = "project4";
-                transform.position = new Vector3(-106, 1.1f, 39);
-                Invoke("check", 0.2f);
-                projectTP.flag4 = false;
-            }
-            else if(project == "project5"){
-                projectName = "project5";
-                transform.position = new Vector3(-115, 1.1f, 39);
-                Invoke("check", 0.2f);
-                projectTP.flag5 = false;
-            }
-            Debug.Log(transform.position);
+        if (projectTP.flag5)
+        {
+            ChangePosition("project5", new Vector3(-113f, 1.1f, 39));
         }
     }
 
-    void check(){
-        Debug.Log(projectName);
-        if(projectName == "project1"){
-            if(transform.position.x != -74){
-                transform.position = new Vector3(-74, 1.1f, 39);
-                Invoke("check", 0.01f);
-            }
-            else{
-                projectTP.flag1 = false;
-            }
+    void ChangePosition(string project, Vector3 position)
+    {
+        if (GetComponent<NetworkObject>().HasStateAuthority)
+        {
+            projectName = project;
+            targetPosition = position;
+            transform.position = position;
+            Debug.Log($"切換至: {project}, 位置: {transform.position}");
+            StartCheck();
         }
-        if(projectName == "project2"){
-            if(transform.position.x != -88){
-                transform.position = new Vector3(-88, 1.1f, 39);
-                Invoke("check", 0.01f);
-            }
-            else{
-                projectTP.flag2 = false;
-            }
+    }
+
+    void StartCheck()
+    {
+        if (!isChecking)
+        {
+            isChecking = true;
+            InvokeRepeating(nameof(CheckPosition), 0.01f, 0.01f);
         }
-        if(projectName == "project3"){
-            if(transform.position.x != -95){
-                transform.position = new Vector3(-95, 1.1f, 39);
-                Invoke("check", 0.01f);
-            }
-            else{
-                projectTP.flag3 = false;
-            }
+    }
+
+    void CheckPosition()
+    {
+        float tolerance = 0.1f; // 容許誤差範圍
+        if (Vector3.Distance(transform.position, targetPosition) > tolerance)
+        {
+            transform.position = targetPosition;
         }
-        if(projectName == "project4"){
-            if(transform.position.x != -106){
-                transform.position = new Vector3(-106, 1.1f, 39);
-                Invoke("check", 0.01f);
-            }
-            else{
-                projectTP.flag4 = false;
-            }
+        else
+        {
+            Debug.Log($"檢查完成，位置穩定於: {transform.position}");
+            CancelInvoke(nameof(CheckPosition));
+            ResetFlags();
+            isChecking = false;
         }
-        if(projectName == "project5"){
-            if(transform.position.x != -115){
-                transform.position = new Vector3(-115, 1.1f, 50);
-                Invoke("check", 0.01f);
-            }
-            else{
-                projectTP.flag5 = false;
-            }
-        }
+    }
+
+    void ResetFlags()
+    {
+        if (projectName == "project1") projectTP.flag1 = false;
+        if (projectName == "project2") projectTP.flag2 = false;
+        if (projectName == "project4") projectTP.flag4 = false;
+        if (projectName == "project5") projectTP.flag5 = false;
     }
 }
-
